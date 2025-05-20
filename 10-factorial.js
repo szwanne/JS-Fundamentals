b4669ba4e6346316a3a6da0798cc1a82f8fbf2da
szwanne <103660814+szwanne@users.noreply.github.com>
2025-05-20 16:45:42 +0200

    Input argument is used, if its an interger it will calculate its factorial

---
+++ 10-factorial.js
@@ -0,0 +1,14 @@
+const [arg] = process.argv.slice(2);
+const number = parseInt(arg);
+
+const factorial = function fact(n) {
+  if (isNaN(n) || n < 0) {
+    return "Invalid input: please enter a non-negative integer";
+  }
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  return n * fact(n - 1);
+};
+
+console.log(factorial(number));
