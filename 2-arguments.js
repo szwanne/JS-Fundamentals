6e653dc7eae9a542f255d8e1a5c2927097845138
szwanne <103660814+szwanne@users.noreply.github.com>
2025-05-20 13:51:19 +0200

    Used an if else statement that prints a message based on the number of arguments passed

---
+++ 2-arguments.js
@@ -0,0 +1,10 @@
+let argument = "";
+
+if ((argument = "")) {
+  console.log("No argument");
+} else if (argument === "Best") {
+  console.log(`${argument} Argument found`);
+} else argument === "Best School";
+{
+  console.log(`${argument} Arguments found`);
+}
